54e672d57c1ea0bf92fb614ff460d8aa7c350c04
dieguis1 <140843734+dieguis1@users.noreply.github.com>
2023-08-17 15:35:28 -0300

    Update load_old_exp.py
    Cargar experimentos anteriores

--- page/pages/load_old_exp.py
+++ page/pages/load_old_exp.py
@@ -1,33 +1,105 @@
 from dash import (
                 Dash, dcc, html, Input, 
                 Output, State ,callback, ALL,
-                register_page,
+                register_page,Patch,no_update,
     )
 import dash_bootstrap_components as dbc
-
-
+import os
+from plotly import graph_objs as go
+# import no_update
+from db_manipulation import utils as dbu
 #----------------------------register--------------------------
-register_page(__name__, path="/load_old",name="Estado actual")
+register_page(__name__, path="/load_old",name="Experimentos anteriores")
 #--------------------------------------------------------------
-
+import sqlite3
 import page_utils
-
+import pandas as pd
 last_exp_data= page_utils.read_exp_file()
 
 
+carpeta_archivos = 'C:\\Users\\Pc\\Downloads\\db_page_ratones-main\\dbs'
 
+def obtener_datos_desde_bd(name :str ):
+    """Leo los datos de la base de datos y devuelvo dict de dataFrames"""
+    conn = sqlite3.connect(name)
+    dfs = {}
+    for i in range(2):
+        df = pd.read_sql_query(f"SELECT * FROM datos_{i}", conn)  # Tablas para los canales sensor/datos_0 a sensor/datos_9
+        dfs[f"datos_{i}"] = df.to_dict()
+    conn.close()
+    return dfs
 
+def trace_from_df(df_dict : dict, i : int):
+    """ trace es la línea que voy a poner en 
+    el plot, lo creo para agregarlo"""
+    df = pd.DataFrame(df_dict)
+    trace = {
+        "x": df["tiempo"],
+        "y": df["valor"],
+        "type": "scatter",
+        "mode": "lines",
+        "name": f"Sensor {i}",
+    }
+    return trace
 
+def create_fig():
+    fig = go.Figure(data=[{"x": [], "y": []} for i in range(2)])
+    fig["layout"] = {"title" : "Datos de Sensores",
+        "xaxis_title" : "Tiempo", 
+        "yaxis_title" : "Valor"
+    }
+    return fig
 
 layout = html.Div([
-     
-    html.Div([ html.H2([ 'acá opción de de cargar un un archivo viejo y mostrar análisis como el otro?'])
-        ],
-        
+    dcc.Interval(
+        id='interval-comp',
+        interval=30 * 1000,  # Actualizar cada 30 segundos
+        n_intervals=0
     ),
+    html.Div([
+        dcc.Dropdown(
+            id='db-dropdown',
+            options=[],
+            value=None,
+            placeholder='Selecciona un archivo de base de datos',
+        ),
+       dcc.Graph(id="selected-graph", figure=create_fig()),
+    ]),
 ])
 
 
-# acá opción de de cargar un un archivo viejo y mostrar análisis con este?
+@callback(
+    Output('db-dropdown', 'options'),
+    Input('interval-comp', 'n_intervals')
+)
+def update_dropdown_options(n):
+    archivos_en_carpeta = [archivo for archivo in os.listdir(carpeta_archivos) if archivo.endswith(".db")]
+    dropdown_options = [{'label': archivo, 'value': archivo} for archivo in archivos_en_carpeta]
+    return dropdown_options
 
+@callback(Output("selected-graph", "figure"),
+              Input("db-dropdown", "value"),
+              )
+def load_graph(value):
+    archivos_en_carpeta = [archivo for archivo in os.listdir(carpeta_archivos) if archivo.endswith(".db")]
+    if value is None:
+        return create_fig()
+    value=value.replace(".db","")
+    patched_fig = create_fig()
 
+    for i in range(2):
+       table_name = f"datos_{i}"
+       data = dbu.obtener_datos_desde_tabla(os.path.join(carpeta_archivos, f"{value}.db"), table_name, step=1)
+       n = value + f"_datos_{i}"
+       this_trace = trace_from_df(data, i)
+        
+        # Si la tabla no está vacía, agrego los datos
+       if this_trace is not None:
+            patched_fig.add_trace(this_trace)
+
+    return  patched_fig
+#  hemos creado un dropdown (dbdropdown) que permite
+#  a los usuarios seleccionar un archivo de base de datos.
+#  Cuando un archivo se selecciona en el dropdown, el callback load_graph
+#  se activa y se encarga de conectarse a la base de datos, obtener los 
+#  datos y crear un gráfico correspondiente.
