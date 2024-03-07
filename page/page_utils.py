2c3a7c909aa958e345cf44d31ed8686a00dfec58
Lucas <140744971+LucasFOviedo@users.noreply.github.com>
2024-03-07 14:32:44 -0300

    Update page_utils.py

--- page/page_utils.py
+++ page/page_utils.py
@@ -15,4 +15,18 @@ def write_exp_file(last_exp_data):
     with open(exp_file, "w") as f:
         json.dump(last_exp_data, f)
     
-
+#def read_exp_file():
+   # last_exp_data = {}
+   # try:
+      #  if os.path.exists(exp_file):
+          #  with open(exp_file, "r") as f:
+         #       file_content = f.read()
+        #        if file_content.strip():  # Verifica si el archivo no está vacío
+       #             last_exp_data = json.loads(file_content)
+      #          else:
+     #               print("El archivo está vacío.")
+    #except FileNotFoundError:
+   #     print(f"El archivo {exp_file} no se encontró.")
+  #  except json.decoder.JSONDecodeError as e:
+ #       print(f"Error de formato JSON en el archivo {exp_file}: {e}")
+#    return last_exp_data 
